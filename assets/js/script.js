abf9ae32eeb18fa8ec33c477c22f3563063e55bb
Selim Elyahyioui <selim.elyahyioui@gmail.com>
2022-07-20 15:35:44 +0200

    HTMLstructure

---
+++ assets/js/script.js
@@ -0,0 +1,327 @@
+const beyoncediscography = [
+  /* ----------------------------------1. Dangerously In Love -------------------------------------- */
+  {
+    album: "Dangerously In Love",
+    releaseYear: 2003,
+    picture:
+      "C:UsersselimOneDriveBureauBecodeCollectionassetsimagespicsDangerously_In_Love_Album(2003).png",
+    length: 3652,
+
+    albumLinks: [
+      {
+        spotifyLink:
+          "https://open.spotify.com/album/06v9eHnqhMK2tbM2Iz3p0Y?si=2jrsYUQDS4qBDbw0M0oDpQ",
+        applemusicLink:
+          "https://music.apple.com/us/album/dangerously-in-love/201274359",
+        deezerLink: "https://deezer.page.link/zExiHjNGNLPZvxrc7",
+        tidalLink: "https://tidal.com/browse/album/1765857",
+      },
+    ],
+
+    trackListing: [
+      {
+        title: "Crazy In Love",
+        timing: 236,
+        featuring: "Jay-Z",
+      },
+      {
+        title: "Nauhgty Girl",
+        timing: 208,
+        featuring: "",
+      },
+      {
+        title: "Baby Bou",
+        timing: 244,
+        featuring: "Sean Paul",
+      },
+      {
+        title: "Hip Hop Star",
+        timing: 222,
+        featuring: "Big Boi & Sleepy Brown",
+      },
+      {
+        title: "Be with You",
+        timing: 260,
+        featuring: "",
+      },
+      {
+        title: "Me Myself and I",
+        timing: 301,
+        featuring: "",
+      },
+      {
+        title: "Yes",
+        timing: 259,
+        featuring: "",
+      },
+      {
+        title: "Signs",
+        timing: 298,
+        featuring: "Missy Elliott",
+      },
+      {
+        title: "Speechless",
+        timing: 360,
+        featuring: "",
+      },
+      {
+        title: "That's How You Like It",
+        timing: 219,
+        featuring: "Jay-Z",
+      },
+
+      {
+        title: "The Closer I Get to You",
+        timing: 297,
+        featuring: "Luther Vandross",
+      },
+
+      {
+        title: "Dangerously In Love 2",
+        timing: 293,
+        featuring: "",
+      },
+      {
+        title: "Beyonce Interlude",
+        timing: 16,
+        featuring: "",
+      },
+      {
+        title: "Gift from Virgo",
+        timing: 163,
+        featuring: "",
+      },
+      {
+        title: "Daddy",
+        timing: 297,
+        featuring: "",
+      },
+    ],
+  },
+  /* --------------------------------------2. B'Day ----------------------------------------- */
+  {
+    album: "B'Day",
+    releaseYear: 2006,
+    picture:
+      "C:UsersselimOneDriveBureauBecodeCollectionassetsimagespicsBeyonce_-_B'Day_(Deluxe)_(cover).png",
+    length: 4150,
+
+    albumLinks: [
+      {
+        spotifyLink:
+          "https://open.spotify.com/album/1L6sJLS18bJTadBupSYFAp?si=cRaoSfOuSCyNlm6Lt7G1pA",
+        applemusicLink:
+          "https://music.apple.com/us/album/bday-deluxe-edition/261707051",
+        deezerLink: "https://deezer.page.link/X6ZG45NvUPjMZtcTA",
+        tidalLink: "https://tidal.com/browse/album/3347090",
+      },
+    ],
+
+    trackListing: [
+      {
+        title: "Beautiful Liar",
+        timing: 199,
+        featuring: "Shakira",
+      },
+      {
+        title: "Irreplaceable",
+        timing: 227,
+        featuring: "",
+      },
+      {
+        title: "Green Light",
+        timing: 210,
+        featuring: "",
+      },
+      {
+        title: "Kitty Kat",
+        timing: 336,
+        featuring: "",
+      },
+      {
+        title: "Welcome to Hollywood",
+        timing: 198,
+        featuring: "",
+      },
+      {
+        title: "Upgrade U",
+        timing: 222,
+        featuring: "Jay-Z",
+      },
+      {
+        title: "Flaws and All",
+        timing: 248,
+        featuring: "",
+      },
+      {
+        title: "World Wide Woman",
+        timing: 221,
+        featuring: "",
+      },
+      {
+        title: "Get Me Bodied (Extended Mix)",
+        timing: 318,
+        featuring: "",
+      },
+      {
+        title: "If",
+        timing: 199,
+        featuring: "",
+      },
+      {
+        title: "Freakum Dress",
+        timing: 200,
+        featuring: "",
+      },
+      {
+        title: "Suga Mama",
+        timing: 204,
+        featuring: "",
+      },
+      {
+        title: "Deja Vu",
+        timing: 240,
+        featuring: "Jay-Z",
+      },
+      {
+        title: "Ring The Alarm",
+        timing: 203,
+        featuring: "",
+      },
+      {
+        title: "Resentment",
+        timing: 280,
+        featuring: "",
+      },
+      {
+        title: "Listen (from the Motion Picture Dreamgirls)",
+        timing: 228,
+        featuring: "",
+      },
+    ],
+  },
+
+  /* --------------------------------------3. I am ... Sasha Fierce ----------------------------------------- */
+
+  {
+    album: "I Am ... Sasha Fierce (Platinum Edition)",
+    releaseYear: 2008,
+    picture:
+      "C:UsersselimOneDriveBureauBecodeCollectionassetsimagespics\1200x1200.jpg",
+    length: 4469,
+
+    albumLinks: [
+      {
+        spotifyLink:
+          "https://open.spotify.com/album/3ROfBX6lJLnCmaw1NrP5K9?si=jZsVm-ktSSG9z5r963LZgQ",
+        applemusicLink:
+          "https://music.apple.com/us/album/i-am-sasha-fierce-deluxe-version/295783816",
+        deezerLink: "https://deezer.page.link/DhV1Ch8esWFhAHXa8",
+        tidalLink: "https://tidal.com/browse/album/3147463",
+      },
+    ],
+
+    trackListing: [
+      {
+        title: "Single Ladies",
+        timing: 192,
+        featuring: "",
+      },
+      {
+        title: "Diva",
+        timing: 200,
+        featuring: "",
+      },
+      {
+        title: "Ego",
+        timing: 236,
+        featuring: "",
+      },
+      {
+        title: "Halo",
+        timing: 261,
+        featuring: "",
+      },
+      {
+        title: "If I Were a Boy",
+        timing: 250,
+        featuring: "",
+      },
+      {
+        title: "Smash into You",
+        timing: 271,
+        featuring: "Jay-Z",
+      },
+      {
+        title: "Sweet Dreams",
+        timing: 208,
+        featuring: "",
+      },
+      {
+        title: "Broken-Hearted Girl",
+        timing: 279,
+        featuring: "",
+      },
+      {
+        title: "Scared of Lonely",
+        timing: 222,
+        featuring: "",
+      },
+      {
+        title: "That's Why You're Beautiful",
+        timing: 221,
+        featuring: "",
+      },
+      {
+        title: "Hello",
+        timing: 256,
+        featuring: "",
+      },
+      {
+        title: "Radio",
+        timing: 218,
+        featuring: "",
+      },
+      {
+        title: "Video Phone",
+        timing: 215,
+        featuring: "Jay-Z",
+      },
+      {
+        title: "Ego (Remix)",
+        timing: 284,
+        featuring: "Kanye West",
+      },
+      {
+        title: "Why Don't You Love Me",
+        timing: 217,
+        featuring: "",
+      },
+      {
+        title: "Honesty",
+        timing: 227,
+        featuring: "",
+      },
+      {
+        title: "Save The Hero",
+        timing: 274,
+        featuring: "",
+      },
+      {
+        title: "Sattelites",
+        timing: 187,
+        featuring: "",
+      },
+      {
+        title: "Disappear",
+        timing: 268,
+        featuring: "",
+      },
+      {
+        title: "Ave Maria",
+        timing: 222,
+        featuring: "",
+      },
+    ],
+  },
+];
